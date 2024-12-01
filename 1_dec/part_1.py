457fa5683305a1cd6a1b69c03cfe3a8e33258480
Thijmen Ketel <thijmenketel@gmail.com>
2024-12-01 14:14:10 +0100

    Cleanup part 1

--- 1_dec/part_1.py
+++ 1_dec/part_1.py
@@ -5,8 +5,9 @@ def get_input_as_int(filename):
         left, right = zip(*[line.split() for line in file])
     return list(map(int, left)), list(map(int, right))
 
-def get_distance(a, b):
-    return abs(a-b)
+def main():
+    left, right = get_input_as_int('./input.txt')
+    print(sum([abs(l-r) for l,r in list(zip(sorted(left), sorted(right)))]))
 
-left, right = get_input_as_int('./input.txt')
-print(sum([get_distance(l, r) for l,r in list(zip(sorted(left), sorted(right)))]))
+if __name__ == "__main__":
+    main()
